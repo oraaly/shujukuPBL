0aedb9e9caa8306f3c5835db1892d79c56da7a78
oraa <743770701@qq.com>
2017-01-15 21:41:11 +0800

    version 1.0

---
+++ WindowsFormsApplication2/WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 72);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "添加记录";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(10, 27);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "创建列";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 151);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(73, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "建立约束";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(114, 110);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "查看视图";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(12, 192);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(88, 23);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "创建存储过程";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(114, 192);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(99, 23);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "使用存储过程";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(12, 110);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(73, 23);
+            this.button11.TabIndex = 10;
+            this.button11.Text = "创建视图";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(114, 72);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(75, 23);
+            this.button12.TabIndex = 11;
+            this.button12.Text = "新建查询";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(114, 27);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(75, 23);
+            this.button14.TabIndex = 13;
+            this.button14.Text = "连接数据库";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(23, 277);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(358, 175);
+            this.textBox1.TabIndex = 14;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 262);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "指令内容";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(413, 27);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(475, 425);
+            this.dataGridView1.TabIndex = 16;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(114, 151);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "删除约束";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(203, 75);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(95, 12);
+            this.label5.TabIndex = 24;
+            this.label5.Text = "Initial Catalog";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(263, 102);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(23, 12);
+            this.label4.TabIndex = 23;
+            this.label4.Text = "uid";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(263, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 12);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "pwd";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(227, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 12);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "Data Source";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(317, 45);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(81, 21);
+            this.textBox2.TabIndex = 25;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(317, 72);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(81, 21);
+            this.textBox3.TabIndex = 26;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(317, 99);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(81, 21);
+            this.textBox4.TabIndex = 27;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(317, 126);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(81, 21);
+            this.textBox5.TabIndex = 28;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(912, 464);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "C#与数据库小练习";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+    }
+}
+
---
+++ WindowsFormsApplication2/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        string strName;
+        SqlCommand cmd = null;  //  数据库连接命令对象。指定执行的SQL语句
+        string sql = null;   // 存放SQL语句的。
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnReg_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+
+        public class Command
+        {
+            public static readonly string ConStr = "Data Source=USER-20141002ZO;Initial Catalog=KTV点歌;uid = sa; pwd = w2aghyc";
+        }
+
+
+        #region 开启数据库
+        private void button14_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection(Command.ConStr))
+            {
+                con.Open();
+                textBox1.Text = Command.ConStr;
+                //strName = "Data Source=USER-20141002ZO;Initial Catalog=KTV点歌;uid = sa; pwd = w2aghyc";
+                //textBox1.Text = strName;
+
+            }
+        }
+        #endregion
+
+        #region 创建表
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // 打开数据库连接
+            using (SqlConnection conn = new SqlConnection(Command.ConStr))
+            {
+                if (conn.State == ConnectionState.Open)
+                    conn.Close();
+                conn.Open();
+                sql = "CREATE TABLE [2015](myId INTEGER CONSTRAINT PKeyMyId PRIMARY KEY,myName CHAR(50) NOT Null, myTable CHAR(255), myValues FLOAT)";
+                textBox1.Text = sql;
+            }
+        }
+        #endregion
+
+        #region 添加记录
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(Command.ConStr))
+            {
+                conn.Open();
+                SqlCommand cmd = null;  //  数据库连接命令对象。指定执行的SQL语句
+                string sql = null;   // 存放SQL语句的。
+                cmd = new SqlCommand(sql, conn);
+                try
+                {
+                    // 向表中添加记录
+                    sql = "INSERT INTO [2015](myId, myName,myAddress,myValues) " + "VALUES (1001, 'Puneet Nehra', 'A 449 Sect 19, DELHI', 23.98 ) ";
+                    cmd = new SqlCommand(sql, conn);
+                    cmd.ExecuteNonQuery();
+                    textBox1.Text = sql;
+                    sql = "INSERT INTO [2015](myId, myName,myAddress, myValues) " + "VALUES (1002, 'Anoop Singh', 'Lodi Road, DELHI', 353.64) ";
+                    cmd = new SqlCommand(sql, conn);
+                    cmd.ExecuteNonQuery();
+                    sql = "INSERT INTO [2015](myId, myName, myAddress, myValues) " + "VALUES (1003, 'Rakesh M', 'Nag Chowk, Jabalpur M.P.', 43.43) ";
+                    cmd = new SqlCommand(sql, conn);
+                    cmd.ExecuteNonQuery();
+                    sql = "INSERT INTO [2015](myId, myName, myAddress, myValues) " + "VALUES (1004, 'Madan Kesh', '4th Street, Lane 3, DELHI', 23.00) ";
+                    cmd = new SqlCommand(sql, conn);
+                    cmd.ExecuteNonQuery();
+                }
+                catch (SqlException ae)
+                {
+                    MessageBox.Show(ae.Message.ToString());
+                }
+            }
+        }
+        #endregion
+
+
+        #region 执行函数
+        private void ExecuteSQLStmt(string sql)
+        {
+            using (SqlConnection conn = new SqlConnection(Command.ConStr))
+            {
+                conn.Open();
+                cmd = new SqlCommand(sql, conn);
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                }
+                catch (SqlException ae)
+                {
+                    MessageBox.Show(ae.Message.ToString());
+                }
+            }
+        }
+        #endregion
+
+
+        #region 添加存储过程
+        private void button8_Click(object sender, EventArgs e)
+        {
+            sql = "CREATE PROCEDURE myProc AS" + " SELECT myName, myAddress FROM [2015] GO";
+            ExecuteSQLStmt(sql);
+            textBox1.Text = sql;
+        }
+        #endregion
+
+        #region 添加视图
+        private void button11_Click(object sender, EventArgs e)
+        {
+            sql = "CREATE VIEW myView AS SELECT myName FROM [2015]";
+            ExecuteSQLStmt(sql);
+            textBox1.Text = sql;
+        }
+        #endregion
+
+
+        #region 新建查询
+        private void button12_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(Command.ConStr))
+            {
+                conn.Open();
+                // 创建数据适配器
+                SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM [2015]", conn);
+                // 创建一个数据集对象并填充数据，然后将数据显示在DataGrid控件中
+                DataSet ds = new DataSet("[2015]");
+                da.Fill(ds, "[2015]");
+                dataGridView1.DataSource = ds.Tables["[2015]"].DefaultView;
+                textBox1.Text = "SELECT * FROM [2015]";
+            }
+
+        }
+        #endregion
+
+        #region 调用存储过程查询
+        private void button9_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(Command.ConStr))
+            {
+                // 创建数据适配器
+                SqlDataAdapter da = new SqlDataAdapter("myProc", conn);
+                // 创建一个数据集对象并填充数据，然后将数据显示在DataGrid控件中
+                DataSet ds = new DataSet("[2015]");
+                da.Fill(ds, "[2015]");
+                dataGridView1.DataSource = ds.Tables["[2015]"].DefaultView;
+                textBox1.Text = "调用存储过程myProc";
+            }
+
+        }
+        #endregion
+
+        #region 查询视图
+        private void button6_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(Command.ConStr))
+            {
+                // 创建数据适配器
+                SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM myView", conn);
+                // 创建一个数据集对象并填充数据，然后将数据显示在DataGrid控件中
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                dataGridView1.DataSource = ds.Tables[0];
+                textBox1.Text = "SELECT * FROM myView";
+            }
+        }
+        #endregion
+
+        #region 创建删除约束
+        private void button3_Click(object sender, EventArgs e)
+        {
+            sql = "CREATE RULE myRule " + "AS @myBalance >= 32 AND @myBalance < 60";
+            ExecuteSQLStmt(sql);
+            textBox1.Text = sql;
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            sql = "DROP RULE myRule";
+            ExecuteSQLStmt(sql);
+            textBox1.Text = sql;
+        }
+        #endregion
+
+
+        #region 索引
+        private void button10_Click(object sender, EventArgs e)
+        {
+            sql = "CREATE UNIQUE INDEX " +"myIdx ON [2015](myName)";
+            ExecuteSQLStmt(sql);
+            textBox1.Text = sql;
+        }
+        #endregion
+
+        #region ALTER添加删除字段
+        private void button5_Click(object sender, EventArgs e)
+        {
+            sql = "ALTER TABLE [2015] ADD " +  "newCol TIMESTAMP";
+            ExecuteSQLStmt(sql);
+            textBox1.Text = sql;
+        }
+        private void button7_Click(object sender, EventArgs e)
+        {
+            sql = "ALTER TABLE [2015] DROP " + "column newCol";
+            ExecuteSQLStmt(sql);
+            textBox1.Text = sql;
+        }
+        #endregion
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
